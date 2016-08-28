aa774cd25b57516c86d24c62a31499efef7f4d15
debasis-devel-007 <debasiskar007@gmail.com>
2016-08-28 23:50:50 +0530

    28 th august

---
+++ app/pages/home/home.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import * as $ from 'jquery'
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(private navCtrl: NavController) {
+
+      /*setTimeout(() => {
+          $("#myCarousel").swiperight(function() {
+              //$(this).carousel('prev');
+          });
+          $("#myCarousel").swipeleft(function() {
+              //$(this).carousel('next');
+              alert(9);
+          });
+      }, 2000);*/
+
+  }
+}
