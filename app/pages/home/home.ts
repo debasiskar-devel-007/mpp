7310c1c4a9db16f08df3356f9b11102a5f130d6a
debasis-devel-007 <debasiskar007@gmail.com>
2016-08-30 03:49:46 +0530

    30th august

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import * as $ from 'jquery'
+import {LoginPage} from '../login/login';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+  loginPage = LoginPage;
   constructor(private navCtrl: NavController) {
 
       /*setTimeout(() => {
